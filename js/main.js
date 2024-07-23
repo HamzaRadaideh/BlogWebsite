6cbc87299dbee9efd6f4a4a0cacd5ccd41e64248
HamzaRadaideh <hjradaideh10@gmail.com>
2024-07-24 01:28:31 +0300

    general improvements

--- js/main.js
+++ js/main.js
@@ -45,3 +45,25 @@ $('.icon-close').click(function () {
     wrapper.removeClass('active active-popup');
     body.css('overflow', 'auto');
 });
+
+
+document.querySelector('.btnLogin-popup').addEventListener('click', () => {
+    document.querySelector('.wrapper').classList.add('active-popup');
+    document.body.style.overflow = 'hidden';
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+document.querySelector('.icon-close').addEventListener('click', () => {
+    document.querySelector('.wrapper').classList.remove('active-popup');
+    document.body.style.overflow = 'auto';
+});
+
+document.querySelector('.register-link').addEventListener('click', () => {
+    document.querySelector('.form-box.login').style.display = 'none';
+    document.querySelector('.form-box.register').style.display = 'block';
+});
+
+document.querySelector('.login-link').addEventListener('click', () => {
+    document.querySelector('.form-box.login').style.display = 'block';
+    document.querySelector('.form-box.register').style.display = 'none';
+});
